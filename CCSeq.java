1f4bb1b299189117acba9b3fe6bb54c0c7eb09a3
FredericoFerreira13 <72949850+FredericoFerreira13@users.noreply.github.com>
2022-05-24 11:09:52 +0100

    Add files via upload

---
+++ CCSeq.java
@@ -0,0 +1,18 @@
+package project;
+public class CCSeq {
+    CounterSequence seq;
+    
+    //Other relevant fields go here...
+    
+    public CCSeq(int cap) {  }
+    
+    public int getCounter(int i) { return 0; }
+    
+    public void incr(int i, int val) {  }
+    
+    public void decr(int i, int val) {  }
+    
+    public int addCounter(int limit) { return 0; }
+    
+    public void remCounter(int i) {  }
+}
---
+++ Counter.java
@@ -0,0 +1,17 @@
+package project;
+public class Counter {
+    
+    private int val;
+    private int limit;
+    private boolean overflow;
+
+    public Counter(int val, int limit) {  }
+
+    public int getVal() { return 0; }
+
+    public int getLimit() { return 0; }
+
+    public void incr(int v) {  }
+
+    public void decr(int v) {  }
+}
