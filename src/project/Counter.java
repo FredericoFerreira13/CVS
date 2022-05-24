873f32371b879dfce9d98845d0b6491a01203de9
FredericoFerreira13 <fma.ferreira@campus.fct.unl.pt>
2022-05-24 17:08:23 +0100

    counter checked in verifast
    counter is done

--- src/project/Counter.java
+++ src/project/Counter.java
@@ -1,18 +1,20 @@
 package src.project;
 
-/*@
-    predicate CounterInv(int v, int l, boolean over) = this.val |-> v &*& this.limit |-> l &*& this.overflow |-> over &*& 0 <= v &*& v < l;
-@*/
+
 
 public class Counter {
     
     private int val;
     private int limit;
     private boolean overflow;
+    
+    /*@
+    predicate CounterInv(int v, int l, boolean over) = this.val |-> v &*& this.limit |-> l &*& this.overflow |-> over &*& 0 <= v &*& v < l;
+@*/
 
     public Counter(int val, int limit) 
     //@ requires val >= 0 &*& val < limit &*& limit > 0;
-    //@ requires CounterInv(val,limit,false);
+    //@ ensures CounterInv(val,limit,false);
     {     
         this.val = val;
         this.limit = limit;
@@ -35,10 +37,10 @@ public class Counter {
 
     public void incr(int v) 
     //@ requires CounterInv(?val,?limit,?overflow) &*& v >= 0;
-    //@ ensures (val + v >= limit)? then CounterInv( (val+v) % limit, limit,true) : CounterInv(val+v,limit,overflow)
+    //@ ensures (val + v >= limit)?  CounterInv( (val+v) % limit, limit,true) : CounterInv(val+v,limit,overflow);
     {  
-        if(val + v >= limit){
-            this.val = (this.val + v) % limit;
+        if(this.val + v >= this.limit){
+            this.val = (this.val + v) % this.limit;
             this.overflow = true;
         }
         else{
@@ -48,9 +50,9 @@ public class Counter {
 
     public void decr(int v) 
     //@ requires CounterInv(?val,?limit,?overflow) &*& v >= 0;
-    //@ ensures (val - v < 0)? then CounterInv(0,limit,true) : CounterInv(val-v,limit,overflow)
+    //@ ensures (val - v < 0)? CounterInv(0,limit,true) : CounterInv(val-v,limit,overflow);
     {
-        if(val - v < 0){
+        if(this.val - v < 0){
             this.val = 0;
             this.overflow = true;
         } 
